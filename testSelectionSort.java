2b382c6854e3dfe740e40dff7b8cb46d9ef91b5b
gkosakow <gkosakow@umich.edu>
2022-04-14 14:29:40 -0400

    prepared to create separate branches

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -3,6 +3,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
+
+	SelectionSort temp1 = new SelectionSort();
+
 	@Test
 	public void test() {
 		testPositive();
@@ -13,20 +16,16 @@ public class testSelectionSort {
 
 	public testSelectionSort() {
 	}
-	
+
 	public void testPositive() {
-		int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
-        
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
+	}
+
+	public void testNegative() {
+	}
+
+	public void testMixed() {
+	}
+
+	public void testDuplicates() {
 	}
 }
